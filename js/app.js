80f80d0435be4793f68498e73e2a504d84591c43
frrf <franciscopali71@gmail.com>
2020-08-02 11:25:02 -0700

    Added primitive item deletion functionality, only deletes last added item, need to make it work on a specific item

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 const userItem = document.getElementById("item");
 const submitButton = document.getElementById("submit-item");
 const outputSection = document.getElementById("output");
+const deleteButton = document.getElementById("delete-item");
 
-const addToList = () => {
+const addToList = () => { //adds item to list
   let added = document.createElement("li");
   added.className = "list-item";
+  added.id = "item";
   added.textContent = userItem.value;
   outputSection.appendChild(added);
 }
 
+const removeFromList = () => {
+  let remove = document.querySelector("li");
+  outputSection.removeChild(remove);
+}
 
 submitButton.addEventListener("click", addToList);
+deleteButton.addEventListener("click", removeFromList)
